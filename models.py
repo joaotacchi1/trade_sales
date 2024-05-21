8f9be2ac86c3d8283a210365f879343f6a1919e5
João Tacchi <tacchijoaovitor@gmail.com>
2024-05-21 17:03:35 -0300

    commit

--- models.py
+++ models.py
@@ -11,6 +11,7 @@ class Product(Base):
     description = Column(String)
     unit_price = Column(Float)
     quantity = Column(Float)
+    obs = Column(String, nullable=False)
     registration_date = Column(Date, default=datetime.now)
 
     sales = relationship('Sale', back_populates='product')
@@ -35,6 +36,7 @@ class Cupom(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement='auto')
     id_venda = Column(Integer, ForeignKey('sales.id')) #chave estrangeira para id da tabela sales
     id_produto = Column(Integer, ForeignKey('products.id')) #chave estrangeira para id da tabela products
+    impression_date = Column(Date, default=datetime.now)
 
     sale = relationship('Sale', back_populates='cupom')
     product = relationship('Product', back_populates='cupom')
