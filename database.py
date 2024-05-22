89ff842d723b69c7ace05f4ef6a50ca983a88db6
João Tacchi <tacchijoaovitor@gmail.com>
2024-05-22 16:18:13 -0300

    Merge branch 'main' of https://github.com/joaotacchi1/trade_sales

--- database.py
+++ database.py
@@ -4,14 +4,10 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
-<<<<<<< Updated upstream
 load_dotenv()
 env = os.getenv('DB_PASSWORD')
 
 DATABASE_URL = "postgresql://postgres:{env}}@localhost:5432/troca"
-=======
-DATABASE_URL = "postgresql://postgres:123@localhost:5432/troca"
->>>>>>> Stashed changes
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
