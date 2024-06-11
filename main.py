12dd8fa84bf49ce7b4b3436c34f937a2cc6f847e
Pedro Figueiredo <pedro_augusto50@hotmail.com>
2024-06-11 14:01:11 -0300

    aasdasdasdasdssadsdasdasdasda

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://localhost",
-    "http://localhost:5173",  # Adicione aqui outras origens permitidas
+    "http://192.168.11.95:3007"  # Adicione aqui outras origens permitidas
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
